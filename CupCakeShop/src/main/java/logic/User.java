ff4d8e76404f2fa74dcbe6f9db901d87096e9a0d
drh89 <42878450+drh89@users.noreply.github.com>
2019-02-28 11:32:22 +0100

    Add files via upload

--- CupCakeShop/src/main/java/logic/User.java
+++ CupCakeShop/src/main/java/logic/User.java
@@ -11,4 +11,73 @@ package logic;
  */
 public class User {
     
+    private String userName;
+    private String password;
+    private String email;
+    private double balance;
+    
+    public User(String userName, String password, String email, double balance){
+        
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.balance = balance;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * @param balance the balance to set
+     */
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
 }
